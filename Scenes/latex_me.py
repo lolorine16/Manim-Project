d63d1e40a338a07fc8bd047ae328a1acfac304a0
Laureen EKON <ekonlaureen16@gmail.com>
2025-09-11 23:56:01 +0000

    Nouveau script

--- Scenes/latex_me.py
+++ Scenes/latex_me.py
@@ -13,3 +13,18 @@ class Fraction(Scene):
 
         self.play(Write(t))
         self.wait(3)
+
+class Equation(Scene):
+    def construct(self):
+        t1 = Tex(r"$y = $", r"$2x + x^2 - 3x + 5$", font_size=96)
+        t2 = Tex(r"$y = $", r"$x^2 - x + 5$", font_size=96)
+
+        g = VGroup(t1, t2).arrange(DOWN, aligned_edge= LEFT, buff = 1)
+
+        self.play(Write(t1))
+        self.wait(1)
+
+        self.play(TransformFromCopy(t1[0], t2[0]))
+        self.play(TransformFromCopy(t1[1], t2[1]))
+
+        self.wait(2)
