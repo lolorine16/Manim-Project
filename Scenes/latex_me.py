f3368833681121dc81dadc138865c5fd2c24ff21
Laureen EKON <ekonlaureen16@gmail.com>
2025-09-11 23:42:49 +0000

    Nouveau script

--- Scenes/latex_me.py
+++ Scenes/latex_me.py
@@ -6,3 +6,10 @@ class MyText(Scene):
         t = Tex(r"$E=mc^2$", font_size = 96)
         self.play(Write(t))
         self.wait(3)
+
+class Fraction(Scene):
+    def construct(self):
+        t = Tex(r"$(\frac{1}{3})^3 - \frac{5}{3} = -1\frac{17}{27}$", font_size= 96)
+
+        self.play(Write(t))
+        self.wait(3)
