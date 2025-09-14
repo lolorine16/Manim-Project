f27db46eeb738a8c3cddd47706bdafb95105f4d4
Laureen EKON <ekonlaureen16@gmail.com>
2025-09-14 20:08:17 +0000

    Nouveau script

---
+++ Scenes/complexe.py
@@ -0,0 +1,11 @@
+from manim import *
+
+class complexe(Scene):
+    def construct(self):
+        t = Text("Ensemble C des nombres complexes")
+        self.play(Write(t))
+        self.wait(3)
+
+
+
+
